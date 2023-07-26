526747415187dc1d17afb8995e646d2ea5dc3774
ShohninDmitriy <demian77neo@mail.ru>
2023-07-27 01:37:38 +0300

    Revert "Update vite.config.inject-version.ts"
    This reverts commit ad2db6dcb5e72c712009090f2c943cd6e98658f4.

--- vite.config.inject-version.ts
+++ vite.config.inject-version.ts
@@ -18,7 +18,7 @@ const writeReleaseInfoFile = async () => {
 
   await releaseInfoFile.writeFile(JSON.stringify({
     project_name: 'fluidd',
-    project_owner: 'ShohninDmitriy',
+    project_owner: 'fluidd-core',
     version: `v${version}`
   }))
 
